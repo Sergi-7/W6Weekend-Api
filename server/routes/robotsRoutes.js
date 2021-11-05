b472a9f466aa7722d98683a54e940d5d38e945ec
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-05 19:14:49 +0100

    Added routes structure

---
+++ server/routes/robotsRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  getRobots,
+  getRobotById,
+  createRobot,
+  updateRobot,
+  deleteRobotById,
+} = require("../controller/robotsController");
+
+const router = express.Router();
+
+router.get("/", getRobots);
+
+router.get("/:idRobot", getRobotById);
+
+router.post("/create", createRobot);
+
+router.put("/update", updateRobot);
+
+router.delete("/delete/:idrobot", deleteRobotById);
